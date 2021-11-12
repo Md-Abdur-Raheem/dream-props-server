6888aaf9a525b81a25cb900b25d413586796a849
Md-Abdur-Raheem <md.raheem2450@gmail.com>
2021-11-12 14:33:33 +0600

    api to delete users order completed

--- index.js
+++ index.js
@@ -60,6 +60,22 @@ async function run() {
             res.json(result);
         })
 
+        //api to get specific users orders
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const filter = { email: email };
+            const result = await ordersCollection.find(filter).toArray();
+            res.json(result);
+        })
+
+        //api to delete users order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
     }
     finally {
         //await client.close()
